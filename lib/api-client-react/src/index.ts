f500ca12205f645aa2db1548edca737ef43f577d
makerdantheman <52270193-makerdantheman@users.noreply.replit.com>
2026-09-04 20:17:22 +0000

    Git commit prior to merge

--- lib/api-client-react/src/index.ts
+++ lib/api-client-react/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./custom-fetch";
 export * from "./generated/api";
 export * from "./generated/api.schemas";
+export * from './generated/api';
+export * from './generated/api.schemas';
